5abd4109e2bfb0359dd634fdbf2f5788d284e561
Lewis <lewis.haris@gmail.com>
2016-04-01 10:48:49 +0800

    Add camera2 fragments

--- app/src/main/java/sg/edu/nus/comp/cs3218_project/MainActivity.java
+++ app/src/main/java/sg/edu/nus/comp/cs3218_project/MainActivity.java
@@ -1,13 +1,20 @@
 package sg.edu.nus.comp.cs3218_project;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, VideoFragment.newInstance())
+                    .commit();
+        }
     }
 }
